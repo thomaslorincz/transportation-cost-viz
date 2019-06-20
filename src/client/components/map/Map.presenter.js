d5fda24aa41b85158e50a0366d24fef31c01b2c6
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-20 08:41:28 -0600

    Add basic map component

---
+++ src/client/components/map/Map.presenter.js
@@ -0,0 +1,16 @@
+import Presenter from '../../superclasses/Presenter';
+
+// eslint-disable-next-line
+export default class MapPresenter extends Presenter {
+  /**
+   * @param {AppModel} model
+   * @param {MapView} view
+   */
+  constructor(model, view) {
+    super(model, view);
+
+    this.view.container.addEventListener('loaded', () => {
+      this.model.initialDraw();
+    });
+  }
+}
---
+++ src/client/components/map/Map.view.js
@@ -0,0 +1,35 @@
+import mapboxgl from 'mapbox-gl';
+import View from '../../superclasses/View';
+
+// eslint-disable-next-line
+export default class MapView extends View {
+  /**
+   * @param {HTMLElement} container
+   */
+  constructor(container) {
+    super(container);
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoidGhvbWFzbG9yaW5jeiIsImEiOiJjamx5aXVwaH' +
+        'AxamZzM3dsaWdkZ3Q2eGJyIn0.mXjlp9c3l2-NBoS1uaEUdw';
+
+    const href = 'href="https://github.com/thomaslorincz"';
+    const rel = 'rel="noopener"';
+    const target = 'target="_blank"';
+    const credit = 'Developed by Thomas Lorincz';
+    const attribution = `<a ${href} ${rel} ${target}>${credit}<a/>`;
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/thomaslorincz/cjx0png073khh1cpap7m6449e',
+      bounds: [[-115.11466, 53.04465], [-112.16116, 54.06214]],
+      attributionControl: false,
+      dragRotate: false,
+      pitchWithRotate: false,
+    }).addControl(new mapboxgl.AttributionControl({
+      customAttribution: attribution,
+    }));
+
+    this.map.on('load', () => {
+      this.container.dispatchEvent(new CustomEvent('loaded'));
+    });
+  }
+}
