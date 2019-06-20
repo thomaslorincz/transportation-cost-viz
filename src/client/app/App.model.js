3a24e258819b7d5f6b7146ee122335f045951a55
Thomas Lorincz <tlorincz@ualberta.ca>
2019-06-20 08:41:18 -0600

    Add basic app model and presenter

---
+++ src/client/app/App.model.js
@@ -0,0 +1,16 @@
+import Model from '../superclasses/Model';
+
+/**
+ * Model that stores and controls the app's data and state.
+ */
+export default class AppModel extends Model {
+  // eslint-disable-next-line
+  constructor() {
+    super();
+  }
+
+  /**
+   * A method for dispatching the initial draw event of the app.
+   */
+  initialDraw() {}
+}
