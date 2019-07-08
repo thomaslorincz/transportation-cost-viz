022de6d1bd688f75017645513e53f72e7f3f96a6
Thomas Lorincz <tlorincz@ualberta.ca>
2019-07-08 11:21:42 -0600

    Add test cases to test dispatch of updateMap events

--- test/test.js
+++ test/test.js
@@ -4,12 +4,40 @@ import EventEmitter from 'eventemitter3';
 import AppModel from '../src/client/app/App.model';
 
 describe('AppModel', () => {
-  describe('#updateProperty', () => {
+  describe('#initialDraw()', () => {
+    it('should dispatch an updateMap event', () => {
+      const emitter = new EventEmitter();
+      const model = new AppModel(emitter);
+
+      let mapUpdated = false;
+      emitter.on('updateMap', () => {
+        mapUpdated = true;
+      });
+
+      model.initialDraw();
+      expect(mapUpdated).to.be.true;
+    });
+  });
+
+  describe('#updateProperty()', () => {
     it('should update the selected property', () => {
       const model = new AppModel(new EventEmitter());
       expect(model.property).to.equal('cost');
       model.updateProperty('proportion');
       expect(model.property).to.equal('proportion');
+    });
+
+    it('should dispatch an updateMap event', () => {
+      const emitter = new EventEmitter();
+      const model = new AppModel(emitter);
+
+      let mapUpdated = false;
+      emitter.on('updateMap', () => {
+        mapUpdated = true;
+      });
+
+      model.initialDraw();
+      expect(mapUpdated).to.be.true;
     });
   });
 });
