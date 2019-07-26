acd4e612f7438f7b4450d190f6b3f9f4b22ac4c8
Thomas Lorincz <tlorincz@ualberta.ca>
2019-07-26 13:13:24 -0600

    Remove unneeded HMR code from client index

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,10 +3,5 @@ import AppModel from './app/AppModel';
 import AppPresenter from './app/AppPresenter';
 import './style.css';
 
-// Needed for Hot Module Replacement
-if (typeof(module['hot']) !== 'undefined') {
-  module['hot'].accept();
-}
-
 const emitter = new EventEmitter();
 new AppPresenter(new AppModel(emitter), null, emitter);
