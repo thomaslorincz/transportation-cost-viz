6f55f4e702682fe51841442a052fb2453e4e6f3c
Thomas Lorincz <tlorincz@ualberta.ca>
2019-07-31 13:38:40 -0600

    Add support for multiple scenarios

--- src/client/app/AppModel.ts
+++ src/client/app/AppModel.ts
@@ -3,6 +3,7 @@ import * as EventEmitter from 'eventemitter3';
 
 /** Model that stores and controls the app's data and state. */
 export default class AppModel extends Model {
+  private scenario: string = 'now';
   private property: string = 'cost';
 
   public constructor(emitter: EventEmitter) {
@@ -11,12 +12,26 @@ export default class AppModel extends Model {
 
   /** A method for dispatching the initial draw event of the app. */
   public initialDraw(): void {
-    this.emitter.emit('updateMap', this.property);
+    this.dispatchDisplayUpdate();
+  }
+
+  /** Update the currently selected scenario. */
+  public updateScenario(scenario: string): void {
+    this.scenario = scenario;
+    this.dispatchDisplayUpdate();
   }
 
   /** Update the currently selected property. */
   public updateProperty(property: string): void {
     this.property = property;
-    this.emitter.emit('updateMap', this.property);
+    this.dispatchDisplayUpdate();
+  }
+
+  /** Dispatch an updateDisplay event. */
+  private dispatchDisplayUpdate(): void {
+    this.emitter.emit('updateDisplay', {
+      scenario: this.scenario,
+      property: this.property,
+    });
   }
 }
--- src/client/app/AppPresenter.ts
+++ src/client/app/AppPresenter.ts
@@ -4,9 +4,12 @@ import MapPresenter from '../components/map/MapPresenter';
 import View from '../superclasses/View';
 import * as EventEmitter from 'eventemitter3';
 import AppModel from './AppModel';
+import LegendView from '../components/legend/LegendView';
+import LegendPresenter from '../components/legend/LegendPresenter';
 
 export default class AppPresenter extends Presenter<AppModel, View> {
   private readonly mapView: MapView;
+  private readonly legendView: LegendView;
 
   public constructor(model: AppModel, view: View, emitter: EventEmitter) {
     super(model, view, emitter);
@@ -14,8 +17,15 @@ export default class AppPresenter extends Presenter<AppModel, View> {
     this.mapView = new MapView(document.getElementById('map'), this.emitter);
     new MapPresenter(this.model, this.mapView, this.emitter);
 
-    this.emitter.on('updateMap', (property: string): void => {
-      this.mapView.draw(property);
+    this.legendView = new LegendView(
+        document.getElementById('legend'),
+        this.emitter
+    );
+    new LegendPresenter(this.model, this.legendView, this.emitter);
+
+    this.emitter.on('updateDisplay', ({scenario, property}): void => {
+      this.mapView.draw(scenario, property);
+      this.legendView.draw(scenario, property);
     });
   }
 }
