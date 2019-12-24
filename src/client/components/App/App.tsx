73c2de4af111c48c95343053b611b2197f83bf83
Thomas Lorincz <tlorincz@ualberta.ca>
2019-12-24 10:25:46 -0700

    Implement StatisticsControl data flow

--- src/client/components/App/App.tsx
+++ src/client/components/App/App.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
-import * as d3 from 'd3-fetch';
+import * as d3 from 'd3';
 import './App.css';
 
 import { MapView } from '../MapView/MapView';
 import { LegendControl } from '../ControlPanel/LegendControl/LegendControl';
-import { StatisticsControl } from '../ControlPanel/StatisticsControl/StatisticsControl';
+import {
+  StatisticsControl,
+  Statistic
+} from '../ControlPanel/StatisticsControl/StatisticsControl';
 import { LoadingScreen } from '../LoadingScreen/LoadingScreen';
+
+interface ConfigEntry {
+  file: string;
+  id: string;
+  label: string;
+}
 
 export interface Household {
   lon: number;
@@ -28,6 +37,7 @@ interface State {
   activeProperty: string;
   colours: string[];
   households: Household[];
+  statistics: Statistic[];
 }
 
 export class App extends React.Component<{}, State> {
@@ -39,20 +49,20 @@ export class App extends React.Component<{}, State> {
     '$750 - $999/month',
     '$1000+/month'
   ];
-  private costRanges = new Map<number, number>([
+  private costRange = [
     [0, 499],
     [500, 749],
     [750, 1000],
     [1000, Number.MAX_SAFE_INTEGER]
-  ]);
+  ];
 
   private proportionLabels = ['0% - 4%', '5% - 9%', '10% - 14%', '15%+'];
-  private proportionRanges = new Map<number, number>([
+  private proportionRange = [
     [0, 4],
     [5, 9],
     [10, 14],
     [15, Number.MAX_SAFE_INTEGER]
-  ]);
+  ];
 
   private scenarioSequence: Map<string, string>;
 
@@ -65,48 +75,47 @@ export class App extends React.Component<{}, State> {
       activeScenario: '',
       activeProperty: 'cost',
       colours: ['#FFCC00', '#FF4111', '#BA1BBA', '#2345B2'],
-      households: []
+      households: [],
+      statistics: []
     };
 
-    Promise.all([d3.csv('./scenario_config.csv')]).then(([config]) => {
+    d3.csv('./scenario_config.csv').then(async config => {
       this.scenarioSequence = new Map<string, string>();
 
       let activeScenario = '';
       let lastEntryId = '';
-      config.forEach(
-        (entry: { file: string; id: string; label: string }, i: number) => {
-          this.scenarios.set(entry.id, {
-            id: entry.id,
-            file: entry.file,
-            label: entry.label,
-            data: []
-          });
-
-          if (i === 0) {
-            activeScenario = entry.id;
-            lastEntryId = entry.id;
-          } else if (i === config.length - 1) {
-            this.scenarioSequence.set(entry.id, config[0].id);
-          } else {
-            this.scenarioSequence.set(lastEntryId, entry.id);
-            lastEntryId = entry.id;
-          }
-        }
-      );
-
-      this.scenarios.forEach((scenario: Scenario) => {
-        d3.csv(scenario.file).then((data: Household[]) => {
-          data.forEach((household: Household) => {
-            scenario.data.push({
-              lon: +household.lon,
-              lat: +household.lat,
-              cost: +household.cost,
-              proportion: +household.proportion
-            });
-          });
+      config.forEach((entry: ConfigEntry, i: number) => {
+        this.scenarios.set(entry.id, {
+          id: entry.id,
+          file: entry.file,
+          label: entry.label,
+          data: []
         });
+
+        if (i === 0) {
+          activeScenario = entry.id;
+          lastEntryId = entry.id;
+        } else if (i === config.length - 1) {
+          this.scenarioSequence.set(entry.id, config[0].id);
+        } else {
+          this.scenarioSequence.set(lastEntryId, entry.id);
+          lastEntryId = entry.id;
+        }
       });
 
+      for (const [id, scenario] of this.scenarios) {
+        const data = await d3.csv(scenario.file);
+        data.forEach((household: Household) => {
+          this.scenarios.get(id).data.push({
+            lon: +household.lon,
+            lat: +household.lat,
+            cost: +household.cost,
+            proportion: +household.proportion
+          });
+        });
+      }
+
+      this.updateStatistics(activeScenario, this.state.activeProperty);
       this.setState({
         loading: false,
         activeScenario,
@@ -115,8 +124,46 @@ export class App extends React.Component<{}, State> {
     });
   }
 
+  private updateStatistics(scenario: string, property: string): void {
+    const data = this.scenarios.get(scenario).data;
+    const range = property === 'cost' ? this.costRange : this.proportionRange;
+
+    const statistics: Statistic[] = [
+      { id: 0, label: '', value: 0 },
+      { id: 1, label: '', value: 0 },
+      { id: 2, label: '', value: 0 },
+      { id: 3, label: '', value: 0 }
+    ];
+
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < range.length; j++) {
+        if (
+          range[j][0] <= data[i][property] &&
+          data[i][property] <= range[j][1]
+        ) {
+          statistics[j].value++;
+        }
+      }
+    }
+
+    if (data.length) {
+      for (let id = 0; id < statistics.length; id++) {
+        statistics[id].value = Math.round(
+          (statistics[id].value / data.length) * 100
+        );
+        statistics[id].label = `${statistics[id].value}%`;
+      }
+    }
+
+    this.setState({ statistics });
+  }
+
   private handleScenarioClicked(scenario: string): void {
-    this.setState({ activeScenario: scenario });
+    this.updateStatistics(scenario, this.state.activeProperty);
+    this.setState({
+      activeScenario: scenario,
+      households: this.scenarios.get(scenario).data
+    });
   }
 
   private handleAnimateClicked(): void {
@@ -124,6 +171,7 @@ export class App extends React.Component<{}, State> {
   }
 
   private handlePropertyClicked(property: string): void {
+    this.updateStatistics(this.state.activeScenario, property);
     this.setState({ activeProperty: property });
   }
 
@@ -163,7 +211,10 @@ export class App extends React.Component<{}, State> {
               this.handleInvertColoursClicked()
             }
           />
-          <StatisticsControl />
+          <StatisticsControl
+            data={this.state.statistics}
+            colours={this.state.colours}
+          />
         </div>
         <LoadingScreen loading={this.state.loading} />
       </React.Fragment>
--- src/client/components/ControlPanel/StatisticsControl/StatisticsControl.tsx
+++ src/client/components/ControlPanel/StatisticsControl/StatisticsControl.tsx
@@ -1,8 +1,65 @@
 import * as React from 'react';
+import * as d3 from 'd3';
 import '../Control.css';
 
-export class StatisticsControl extends React.Component<{}, {}> {
+export interface Statistic {
+  id: number;
+  label: string;
+  value: number;
+}
+
+interface Props {
+  data: Statistic[];
+  colours: string[];
+}
+
+export class StatisticsControl extends React.Component<Props, {}> {
+  private readonly width: number = 240;
+  private readonly height: number = 240;
+  private readonly margin: number = 10;
+
   public render(): React.ReactNode {
+    const { data, colours } = this.props;
+
+    const x = d3
+      .scaleBand()
+      .domain(data.map((datum): string => datum.id.toString()))
+      .range([0, this.width])
+      .padding(0.2);
+
+    const y = d3
+      .scaleLinear()
+      .domain([0, 50])
+      .range([this.height, this.margin]);
+
+    const svg = d3.select('#bar-chart');
+
+    svg
+      .selectAll('rect')
+      .data(data)
+      .join('rect')
+      .attr('x', (datum): number => x(datum.id.toString()))
+      .attr('width', x.bandwidth())
+      .transition()
+      .duration(1000)
+      .attr('fill', (datum, i): string => colours[i])
+      .attr('y', (datum): number => y(datum.value))
+      .attr('height', (datum): number => this.height - y(datum.value));
+
+    svg
+      .selectAll('text')
+      .data(data)
+      .join('text')
+      .attr('font-family', '"Montserrat", sans-serif')
+      .attr('x', (datum): number => {
+        return x(datum.id.toString()) + x.bandwidth() / 2;
+      })
+      .attr('text-anchor', 'middle')
+      .text((datum): string => datum.label)
+      .transition()
+      .duration(1000)
+      .attr('y', (datum): number => y(datum.value) + 2 * this.margin);
+
     return (
       <div className="control statistics-control">
         <div className="title">Statistics</div>
